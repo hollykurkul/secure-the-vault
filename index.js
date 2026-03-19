910666817b9b498f43ed21d5eacfe2230e4989a8
Holly K <hollkurk@gmail.com>
2026-03-18 21:59:56 -0400

    add index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+let code1;
+console.log(code1);
+
+code1 = 7 + 3;
+console.log(code1);
+
+let code2 = (100 - 20) / 2;
+console.log(code2);
+
+let code3 = 39 % 40;
+console.log(code3);
+
+let message = "The vault has been secured. The combination is:";
+console.log(message);
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+console.log(codeA);
+
+let codeB = `${code1}-${code2}-${code3}`;
+console.log(codeB);
+
+console.log(message, codeA, codeB);
